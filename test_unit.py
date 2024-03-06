166cac8f2eb0777bfbb9110dad4e5250ef980917
DeriDreri <dominikcybulski@protonmail.com>
2024-03-06 14:47:02 +0100

    Add simple test for proper alarm detection

--- test_unit.py
+++ test_unit.py
@@ -1,5 +1,7 @@
 import check 
 
+#Tests should include more edge cases
+
 def test_default_config():
 
     #Test for right detection of bad config file
@@ -18,3 +20,9 @@ def test_get_api_string():
     #No default base currency
     testMap = {"baseCurrency": "PLN", "currencies": [{"name":"EUR"}] }
     assert check.createApiString("X", testMap) == "https://api.freecurrencyapi.com/v1/latest?apikey=X&currencies=EUR&base_currency=PLN", "Different currency failed"
+
+def test_analyze_response():
+    #Test for single alarm raised
+    requestMap = {"baseCurrency": "USD", "currencies": [{"name":"EUR"},{"name":"PLN", "alarm":"4.0"},{"name":"GBR", "alarm":"2.0"}] }
+    responseMap = {"EUR":"2.5","PLN":"3.9","GBR":"4.0"}
+    assert check.analyzeApiResponse(requestMap, responseMap) == "Alarm! You can buy 4.0 of GBR for USD\n"
